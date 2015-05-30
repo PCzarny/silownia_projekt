b44be1c1f06594590f3de04bce68e239e224af0b
sztukamichal <sztuuka.michal@gmail.com>
2015-05-30 19:05:00 +0200

    fix

--- silownia_java/src/silownia_java/Constants.java
+++ silownia_java/src/silownia_java/Constants.java
@@ -2,8 +2,8 @@ package silownia_java;
 
 public class Constants {
     public static String dbClass = "com.mysql.jdbc.Driver";
-    private static String dbName= "silownia";
+    private static String dbName= "silownia_baza";
     public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
     public static String dbUser = "root";
-    public static String dbPwd = "admin";
+    public static String dbPwd = "";
 }
