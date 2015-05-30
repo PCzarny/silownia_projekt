cb4ec4fec0e571dab7fcfc40f3d563e68a3f9478
barni491 <barni491@gmail.com>
2015-05-30 21:49:15 +0200

    add_user_dimmension

--- silownia_java/src/dao/DimensionDAO.java
+++ silownia_java/src/dao/DimensionDAO.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import silownia_java.DBConnection;
 import model.Dimension;
+
 
 
 public class DimensionDAO {
@@ -106,6 +108,41 @@ public class DimensionDAO {
 		 return dimensions;
 		
 	}
+	
+	
+public static void addUserDimmension(Dimension dimension) throws SQLException{
+		
+		Connection dbConn = null;
+		String sql = "call add_user_dimmension(?, ?, ?, ?);";
+	     try {
+	    	 try{
+	    		 dbConn = DBConnection.createConnection();
+	             System.out.println(dbConn);
+	            } 
+	    	 	catch (Exception e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	    	 	java.sql.PreparedStatement ps = dbConn.prepareStatement(sql);
+	    	 	ps.setInt(1, dimension.getUser_id());
+	    	 	ps.setInt(2, dimension.getDimension_id());
+	    	 	ps.setInt(3, dimension.getValue());
+	    	 	ps.setDate(4, dimension.getCreated_on());
+	    	 	ps.execute();
+	        } catch (SQLException sqle) {
+	            throw sqle;
+	        } catch (Exception e) {
+	            // TODO Auto-generated catch block
+	            if (dbConn != null) {
+	                dbConn.close();
+	            }
+	            throw e;
+	        } finally {
+	            if (dbConn != null) {
+	                dbConn.close();
+	            }
+	        }		
+	}	
 	
 
 }
--- silownia_java/src/silownia_java/DimensionREST.java
+++ silownia_java/src/silownia_java/DimensionREST.java
@@ -3,11 +3,14 @@ package silownia_java;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import model.*;
 import dao.DimensionDAO;;
@@ -43,5 +46,24 @@ public class DimensionREST {
     }
 	
 	
+    
+ 
+    @POST
+    @Path("/addUserDimension")
+    @Consumes(MediaType.APPLICATION_JSON)
+    
+    //JSON {"user_dimension_id":null,"dimension_id":1,"name":"waga","value":120,"unit":"kg","user_id":1,"created_on":"2015-04-30"}
+    // http://localhost:8080/silownia_java/rest/dimension/addUserDimension
+    
+    
+    public Response addUserDimension(Dimension dimension) throws SQLException{
+    	
+    	
+    	DimensionDAO.addUserDimmension(dimension);
+    	
+    	return Response.status(200).entity("Powodzenie").build();
+    }
+    
+    
 	
 }
