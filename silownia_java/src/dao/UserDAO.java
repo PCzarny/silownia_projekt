4cb61e12bccc82a4cac6dd978a1a21b429f41806
sztukamichal <sztuuka.michal@gmail.com>
2015-06-02 00:26:36 +0200

    dodanie logowania

--- silownia_java/src/dao/UserDAO.java
+++ silownia_java/src/dao/UserDAO.java
@@ -178,4 +178,47 @@ public class UserDAO {
 	            }
 	        }		
 	}
+
+	public static User login(String login, String password) throws SQLException{
+		 Connection dbConn = null;
+		 
+		 User user = new User();
+	        try {
+	            try {
+	                dbConn = DBConnection.createConnection();
+	                System.out.println(dbConn);
+	            } catch (Exception e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	            Statement stmt = dbConn.createStatement();
+	            String query = "SELECT * FROM user WHERE login = '" + login+"' and password = '"+password+"'";
+	            System.out.println(query);
+	            ResultSet rs = stmt.executeQuery(query);
+	            while (rs.next()) {
+	             user.setUserId(rs.getInt(1));
+	             user.setName(rs.getString(2));
+	             user.setSurname(rs.getString(3));
+	             user.setLogin(rs.getString(4));
+	             user.setPassword(rs.getString(5));
+	             user.setEmail(rs.getString(6));
+	             user.setCreate_on(rs.getDate(7));
+	            }
+	        } catch (SQLException sqle) {
+	            throw sqle;
+	        } catch (Exception e) {
+	            // TODO Auto-generated catch block
+	            if (dbConn != null) {
+	                dbConn.close();
+	            }
+	            throw e;
+	        } finally {
+	            if (dbConn != null) {
+	                dbConn.close();
+	            }
+	        }
+		 
+		 return user;
+	}
+
 }
