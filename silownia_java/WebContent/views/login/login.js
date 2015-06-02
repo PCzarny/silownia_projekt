31690b290c2e857b0a6314f96f55afc5198e5aab
Piotr Czarny <czarny-piotr@wp.pl>
2015-06-02 02:21:02 +0200

    próba logowania

--- silownia_java/WebContent/views/login/login.js
+++ silownia_java/WebContent/views/login/login.js
@@ -11,8 +11,8 @@
 		}])
 		.controller('LoginController', LoginController);
 
-	LoginController.$inject = ['$scope', '$rootScope', 'AuthService', 'UserService', '$location'];
-	function LoginController($scope, $rootScope, AuthService, UserService, $location) {
+	LoginController.$inject = ['$scope', '$rootScope', 'AuthService', 'UserService', '$location', '$http'];
+	function LoginController($scope, $rootScope, AuthService, UserService, $location, $http) {
 		$scope.errorMessage = false;
 		
 		var vm = this;
@@ -20,28 +20,41 @@
 		UserService.setUser(undefined);
 
 		$('#login-button').on('click', function () {
-			var credentials = {
-				username: $('#input-username').val(),
-				password: $('#input-password').val()
-			};
+//			var credentials = {
+//				username: $('#input-username').val(),
+//				password: $('#input-password').val()
+//			};
+//
+//			AuthService.login(credentials).then(function (data) {
+//				console.log(data);
+//				if ( data.status == true){
+//					var user = {
+//							id : 1,
+//							username: credentials.username
+//						}
+//					UserService.setUser(user);
+//					$rootScope.$broadcast('SetUser');
+//					$location.url('/home');
+//					console.log('Poprawnie zalogowano: ' + user.username);
+//				}
+//				else{
+//					$scope.errorMessage = 'Wrong username or password';
+//					console.log('niepoprawne logowanie');
+//				}	
+//			});
+			var credencials = ($(".form-login").serialize());
+			console.log(credencials);
 
-			AuthService.login(credentials).then(function (data) {
-				console.log(data);
-				if ( data.status == true){
-					var user = {
-							id : 1,
-							username: credentials.username
-						}
-					UserService.setUser(user);
-					$rootScope.$broadcast('SetUser');
-					$location.url('/home');
-					console.log('Poprawnie zalogowano: ' + user.username);
-				}
-				else{
-					$scope.errorMessage = 'Wrong username or password';
-					console.log('niepoprawne logowanie');
-				}	
-			});
+			
+			$http.post('./rest/user/loginUser', credencials).
+			  success(function(data, status, headers, config) {
+			    console.log(status);
+			  }).
+			  error(function(data, status, headers, config) {
+			    console.log("Błąd");
+			    console.log(status);
+			  });
+			//var object = $.extend({}, credencials, "userId":null,"name":null,"surname":null,"email":null,"create_on":null});
 		});
 		
 		$('#register-button').on('click',function(){
