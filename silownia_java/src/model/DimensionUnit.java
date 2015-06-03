bae1d4533faa8eed0352ffa03ce71045e1316343
barni491 <barni491@gmail.com>
2015-06-03 13:35:41 +0200

    UNITDimension

---
+++ silownia_java/src/model/DimensionUnit.java
@@ -0,0 +1,27 @@
+package model;
+
+public class DimensionUnit {
+	
+	private int dimensionId;
+	private String name ;
+	private String unit ;
+	public int getDimensionId() {
+		return dimensionId;
+	}
+	public void setDimensionId(int dimensionId) {
+		this.dimensionId = dimensionId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	} 
+
+}
--- silownia_java/src/silownia_java/ExerciseREST.java
+++ silownia_java/src/silownia_java/ExerciseREST.java
@@ -34,6 +34,8 @@ public class ExerciseREST {
 	}
 	
 	
+	
+	
 	@GET
     
     @Path("/UserExercises")
