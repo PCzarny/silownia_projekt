5432be69e71b2b7a4799186c0afec10b4f13764e
sztukamichal <sztuuka.michal@gmail.com>
2015-06-03 14:04:25 +0200

    odpinanie przypinanie

--- silownia_java/src/silownia_java/Constants.java
+++ silownia_java/src/silownia_java/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     private static String dbName= "silownia";
     public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
     public static String dbUser = "root";
-    public static String dbPwd = "admin";
+    public static String dbPwd = "";
 }
